bc98f1549fbf4abbd7f6a8c88e2ff86ee6ac611c
sungalex <cloudallera@gmail.com>
2019-05-15 16:21:54 +0900

    print문 주석 처리
    Signed-off-by: sungalex <cloudallera@gmail.com>

--- 16.contour_0.py
+++ 16.contour_0.py
@@ -1,4 +1,4 @@
-# contour : 테투리(윤관선)
+# contour : 테투리(윤곽선)
 import cv2
 import numpy as np
 
@@ -13,12 +13,12 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 _, img_thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)   # threshold(150) 보다 큰 값은 255로, 작은 값은 0으로 변환
 
 contours, _ = cv2.findContours(img_thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)   # RETR: retrieval, APPROX: Aproximation(근사)
-print(type(contours[0]))
-print('contours')
-for c in contours:
-    print(c.shape, c.dtype, cv2.contourArea(c))   # c.shape : (점의 갯수, 1, (x,y))
+# print(type(contours[0]))
+# print('contours')
+# for c in contours:
+#     print(c.shape, c.dtype, cv2.contourArea(c))   # c.shape : (점의 갯수, 1, (x,y))
 
-print(contours[0][100][0][0], contours[0][100][0][1])    # 첫번째 contour의 101번째 x, y 좌표
+# print(contours[0][100][0][0], contours[0][100][0][1])    # 첫번째 contour의 101번째 x, y 좌표
 
 color = [(0,255,0), (0,0,255)]
 for idx, contour in enumerate(contours):
